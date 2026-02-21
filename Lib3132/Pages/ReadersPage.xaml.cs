81165fd2e6dabda951b5bc99e735663589aae0b2
Kumyshbaeva Sofiya <kimsofocka10@gmail.com>
2026-02-21 16:52:30 +0300

    добавили поиск, сортировку, фильтраци.

--- Lib3132/Pages/ReadersPage.xaml.cs
+++ Lib3132/Pages/ReadersPage.xaml.cs
@@ -22,12 +22,45 @@ namespace Lib3132.Pages
     public partial class ReadersPage : Page
     {
         public static List<Reader> readers {  get; set; }
+        public static List<string> sorts { get; set; }
+        public static List<Gender> genders { get; set; }
         public ReadersPage()
         {
             InitializeComponent();
             readers = new List<Reader>(ConnectionString.libraryKIUEntities.Reader.ToList());
-
+            sorts = new List<string> { "Сбросить сортировку", "А - Я", "Я - А" };
+            SortCmb.ItemsSource = sorts;
+            genders = new List<Gender>(ConnectionString.libraryKIUEntities.Gender.ToList());
             this.DataContext = this;
+        }
+
+        private void SearchTb_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (SearchTb.Text != "")
+                ReadersLv.ItemsSource = new List<Reader>(ConnectionString.libraryKIUEntities.Reader.
+                Where(i => i.FIO.Contains(SearchTb.Text)).ToList());
+            else
+                ReadersLv.ItemsSource = readers;
+        }
+
+        private void SortCmb_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (SortCmb.SelectedItem.ToString() == "А - Я")
+                ReadersLv.ItemsSource = new List<Reader>(ConnectionString.libraryKIUEntities.Reader.
+                    OrderBy(i => i.FIO).ToList());
+            else if(SortCmb.SelectedItem.ToString() == "Я - А")
+                ReadersLv.ItemsSource = new List<Reader>(ConnectionString.libraryKIUEntities.Reader.
+                    OrderByDescending(i => i.FIO).ToList());
+            else
+                ReadersLv.ItemsSource = readers;
+
+        }
+
+        private void FilterCmb_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var gen = FilterCmb.SelectedItem as Gender;
+            ReadersLv.ItemsSource = new List<Reader>(ConnectionString.libraryKIUEntities.Reader.
+                Where(i => i.IdGender == gen.Id).ToList());
         }
     }
 }
